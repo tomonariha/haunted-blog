896c44113c628e4fd735b2c9bf5e0a48695d8698
tomonari <zcsxcacg@i.softbank.jp>
2022-06-05 14:29:05 +0900

    rubocop対応

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -17,7 +17,7 @@ class Blog < ApplicationRecord
 
   scope :default_order, -> { order(id: :desc) }
 
-  scope :owned, -> (current_user){ where('user_id = ?', current_user) }
+  scope :owned, ->(current_user) { where('user_id = ?', current_user) }
 
   def owned_by?(target_user)
     user == target_user
